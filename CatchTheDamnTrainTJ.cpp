25b0d0f07adea51be21c598dcdcd0b7b1b1973df
Nandoascal <fxa6511@rit.edu>
2017-09-03 13:42:06 -0400

    added cpp
    added cpp

---
+++ CatchTheDamnTrainTJ.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <limits>
+#include <cmath>
+#undef max
+
+using namespace std;
+
+
+int main()
+{
+	double velocity,
+           efficiency,
+           mass0,
+		   mass1;
+    //velocity is delta v, efficiency is v_e, mass0 is initial mass, mass1 is final mass
+
+
+	
+	cout << "T.J. enter your velocity: ";
+	cin >> velocity;
+	while(!cin) //input validation, could've been put in a function, but ehhhhhh copy pasta is cool
+	{
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		cout << "Wrong input! Input a new one: ";
+		cin >> velocity;
+	}
+	
+	cout << "T.J. enter your efficiency: ";
+	cin >> efficiency;
+	while(!cin)
+	{
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		cout << "Wrong input! Input a new one: ";
+		cin >> efficiency;
+	}
+	
+	cout << "T.J. enter the mass: ";
+	cin >> mass1;
+	while(!cin)
+	{
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		cout << "Wrong input! Input a new one: ";
+		cin >> mass1;
+	}
+	
+	mass0 = mass1 * exp(velocity/(9.8 * efficiency)); 
+	
+	cout << "Your initial mass should be: " << mass0;
+	
+	return 0;
+}
